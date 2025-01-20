8f88009918a0d37ca548958c05c64b2382d36935
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-20 12:43:39 -0500

    Add project files.

---
+++ W25Week3IntroToInheritance/BaseClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W25Week3IntroToInheritance
+{
+    public class BaseClass
+    {
+        private int myPrivateVar;
+        protected int myProtectedVar;
+        public int myPublicVar;
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public void MyMethod()
+        {
+            //myPrivateVar = 1;
+            myProtectedVar = 1;
+            myPublicVar = 1;
+        }
+    }
+}
---
+++ W25Week3IntroToInheritance/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace W25Week3IntroToInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass objBase = new BaseClass();
+            objBase.myPublicVar = 1;
+
+            DerivedClass objDerived = new DerivedClass();
+            objDerived.myPublicVar = 2;
+        }
+    }
+}
